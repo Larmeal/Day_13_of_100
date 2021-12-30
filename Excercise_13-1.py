048607559ef43a742fd5faa7b93289bf5dd105f8
Larmeal <armloveyou252@hotmail.com>
2021-12-30 20:46:16 +0700

    Easy day

---
+++ Excercise_13-1.py
@@ -0,0 +1,16 @@
+# ฺBug ที่ 1
+# Problem 1
+# number = int(input("Which number do you want to check?"))
+
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+# Solution 1 
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
---
+++ Excercise_13-2.py
@@ -0,0 +1,31 @@
+# ฺBug ที่ 2
+# Problem 2
+
+# year = input("Which year do you want to check?")
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+
+# Solution 2
+
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+  
